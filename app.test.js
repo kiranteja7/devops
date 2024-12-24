99c3bd9728d0a7c804d882c079e8e03a04f2f9d1
kiranteja7 <88424099+kiranteja7@users.noreply.github.com>
2024-12-24 08:53:15 +0530

    app.test.js  changes

--- app.test.js
+++ app.test.js
@@ -1,11 +1,33 @@
-import request from 'supertest';
-import { expect } from 'chai';
-import app from './index.js';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app'); // Assuming this is your server file
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+let app;
+
+before((done) => {
+    app = server.listen(3000, () => {
+        console.log('Test server running on port 3000');
+        done();
+    });
+});
+
+after((done) => {
+    app.close(() => {
+        console.log('Test server closed');
+        done();
+    });
+});
 
 describe('GET /', () => {
-    it('should return Hello World', async () => {
-        const res = await request(app).get('/');
-        expect(res.text).to.equal('Hello World');
-        expect(res.statusCode).to.equal(200);
+    it('should return Hello World', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res.text).to.equal('Hello World');
+                done();
+            });
     });
 });
